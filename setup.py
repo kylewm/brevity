61f914175d6fa8df584017e1bf3c08db4b1bbe53
Kyle Mahan <kyle.mahan@gmail.com>
2015-03-30 18:26:45 +0000

    setup.py read long_desc from README.rst

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+def readme():
+    with open(os.path.join(os.path.dirname(__file__), 
+                           'README.rst')) as f:
+        return f.read()
+
 
 setup(name='brevity',
-      version='0.1.1',
+      version='0.1.2',
       description='Tweet shortening utility',
-      long_description='Utility for shortening longer notes to at most 140 characters.',
+      long_description=readme(),
       author='Kyle Mahan',
       author_email='kyle@kylewm.com',
       url='http://indiewebcamp.com/brevity',
